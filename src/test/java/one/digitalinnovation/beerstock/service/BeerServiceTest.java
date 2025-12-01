7d0c365cc8d6ece2d0fadd86973f5a7796002cc7
reginaldocoutinho <reginaldo_re12@hotmail.com>
2025-12-01 14:36:42 -0300

    teste pipeline

--- src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
+++ src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
@@ -36,7 +36,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-//teste pipeline
+//teste pipeline 2
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
 
